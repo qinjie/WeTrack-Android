7416e854521811162a5fd4e90ed116094bd8092a
mark.qj <mark.qj@gmail.com>
2017-01-25 15:17:23 +0800

    Bug Fix: BeaconInfo class needs to implement Parcelable

--- app/src/main/java/edu/np/ece/wetrack/model/BeaconInfo.java
+++ app/src/main/java/edu/np/ece/wetrack/model/BeaconInfo.java
@@ -5,7 +5,7 @@ import android.os.Parcelable;
 
 import com.google.gson.annotations.SerializedName;
 
-public class BeaconInfo {
+public class BeaconInfo implements Parcelable {
     @SerializedName("id")
     private int id;
 
